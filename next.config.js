3c616535ce63fac7f6d7dea1439e37c279d4ca16
Nick Ray <nray@nray.io>
2022-09-10 23:31:40 -0600

    Add experiment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withContentlayer({
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
   reactStrictMode: false,
   swcMinify: true,
   webpack(config) {
