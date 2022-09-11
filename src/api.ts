0dc34f97c9cb35b29c935cb53798df6e5313a4aa
Nick Ray <nray@nray.io>
2022-09-10 21:43:08 -0600

    Revise eslint

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,4 @@
-import { Post } from "contentlayer/generated";
-import { allPosts as allPostsExternal } from "contentlayer/generated";
+import { Post, allPosts as allPostsExternal } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
 
 /**
@@ -13,21 +12,31 @@ const pick = <Obj, Keys extends keyof Obj>(obj: Obj, keys: Keys[]) => {
     }
     return newObj;
   }, {} as Pick<Obj, typeof keys[number]>);
-}
+};
 
 /**
  * @param fields List of fields to include in the post.
  * @param limit Maximum number of posts returned.
  */
-export const allPosts = <Keys extends keyof Post>(fields: Keys[], limit?: number) => {
-  return allPostsExternal
-    .filter((post) => process.env.NODE_ENV === "development" ? true : post.status === "published")
-    .sort((a, b) => {
-      return compareDesc(new Date(a.publishedAt || new Date()), new Date(b.publishedAt || new Date()));
-    })
-    .slice(0, limit ? limit + 1 : limit)
-    // Only pass data that the client actually uses.
-    .map((post) => (
-      pick(post, fields)
-    ));
-}
+export const allPosts = <Keys extends keyof Post>(
+  fields: Keys[],
+  limit?: number
+) => {
+  return (
+    allPostsExternal
+      .filter((post) =>
+        process.env.NODE_ENV === "development"
+          ? true
+          : post.status === "published"
+      )
+      .sort((a, b) => {
+        return compareDesc(
+          new Date(a.publishedAt || new Date()),
+          new Date(b.publishedAt || new Date())
+        );
+      })
+      .slice(0, limit ? limit + 1 : limit)
+      // Only pass data that the client actually uses.
+      .map((post) => pick(post, fields))
+  );
+};
