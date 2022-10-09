2d44024356f9e59cf3ca5380f2d0d0a2eac7e164
Nick Ray <nray@nray.io>
2022-10-09 11:23:35 -0600

    Remove cra

--- content/blog/how-to-create-performant-scroll-animations-in-react/demos/react-scroll-animations/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as ReactDOMClient from "react-dom/client";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement as Element);
-
-root.render(<App />);
