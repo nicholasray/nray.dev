b23401557a8fd7792aba7dc0846961f62d56a9d5
Nick Ray <nray@nray.io>
2022-10-08 19:18:55 -0600

    Set layout to block

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -8,13 +8,12 @@ function CodeEditor(props: CodeEditorProps) {
     <Sandpack
       template="react-ts"
       theme={nightOwl}
-      // options={{
-      //   classes: {
-      //     "sp-layout": "flex-col h-[700px]",
-      //     "sp-preview-iframe": "!min-h-[350px]",
-      //   },
-      //   editorHeight: 350,
-      // }}
+      options={{
+        classes: {
+          "sp-layout": "!block",
+        },
+        editorHeight: 350,
+      }}
       {...props}
     />
   );
