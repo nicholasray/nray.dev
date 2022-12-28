333ad5a133555cbfbf04214049d97ab06d19ec17
Nick Ray <nray@nray.io>
2022-12-28 11:51:37 -0700

    Add syntax

--- astro.config.ts
+++ astro.config.ts
@@ -7,11 +7,36 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from "./remark/remarkReadingTime";
+import rehypePrettyCode from "rehype-pretty-code";
+import ayu from "./src/themes/ayu.json";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
+    syntaxHighlight: false,
     remarkPlugins: [remarkReadingTime],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: ayu,
+          onVisitLine(node: any) {
+            // Prevent lines from collapsing in `display: grid` mode, and
+            // allow empty lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          // Feel free to add classNames that suit your docs
+          onVisitHighlightedLine(node: any) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node: any) {
+            node.properties.className = ["word"];
+          },
+        },
+      ],
+    ],
     extendDefaultPlugins: true,
   },
   integrations: [react(), tailwind(), mdx()],
