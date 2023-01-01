05afdc3827ba9b2946a5383b6b74737bef7be2f0
Nicholas Ray <nray@nray.dev>
2023-01-01 11:25:27 -0700

    Add partytown and analytics

--- astro.config.ts
+++ astro.config.ts
@@ -11,6 +11,7 @@ import rehypePrettyCode from "rehype-pretty-code";
 import ayu from "./src/themes/ayu.json";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -70,6 +71,11 @@ export default defineConfig({
     mdx(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
     }),
   ],
   vite: {
