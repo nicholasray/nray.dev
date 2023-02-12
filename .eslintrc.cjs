8c81d8b77a397c8b7839ea97f1df3b9c781a6993
Nicholas Ray <nray@nray.dev>
2023-02-12 14:15:23 -0700

    Support tsx in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -47,7 +47,7 @@ module.exports = {
       },
     },
     {
-      files: ["*.ts"],
+      files: ["*.ts", "*.tsx"],
       parser: "@typescript-eslint/parser",
       extends: ["plugin:@typescript-eslint/recommended"],
       rules: {
