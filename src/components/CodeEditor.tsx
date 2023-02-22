8c5caab8396f36cefa1a554fae0666c0b3a3ad9c
Nicholas Ray <nray@nray.dev>
2023-02-22 13:17:13 -0700

    Update deps

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,12 +1,11 @@
 import { Sandpack, SandpackProps } from "@codesandbox/sandpack-react";
-import { sandpackDark } from "@codesandbox/sandpack-themes";
 
 type CodeEditorProps = SandpackProps;
 
 function CodeEditor(props: CodeEditorProps) {
   return (
     <Sandpack
-      theme={sandpackDark}
+      theme={"auto"}
       options={{
         resizablePanels: false,
       }}
