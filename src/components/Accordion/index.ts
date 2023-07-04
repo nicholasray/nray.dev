47ca0b0870cda6d13ba095ce7932a2b7c6c1d12a
Nicholas Ray <nray@nray.dev>
2023-07-04 00:31:01 -0600

    Update

---
+++ src/components/Accordion/index.ts
@@ -0,0 +1,7 @@
+import Root from "./Accordion.astro";
+import Content from "./AccordionContent.astro";
+import Header from "./AccordionHeader.astro";
+import Item from "./AccordionItem.astro";
+import Trigger from "./AccordionTrigger.astro";
+
+export { Root, Content, Header, Item, Trigger };
