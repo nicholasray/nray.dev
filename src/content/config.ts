82b5c40aa5ded35d601a2075cb466e4a77a0ca85
Nicholas Ray <nray@nray.dev>
2023-07-19 13:53:57 -0600

    Change content collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,17 +3,18 @@ import { z, defineCollection } from "astro:content";
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
   type: "content", // v2.5.0 and later
-  schema: z.object({
-    title: z.string(),
-    description: z.string().max(160),
-    publishedAt: z.date().optional(),
-    tags: z.array(z.string()).optional(),
-    cover: z.object({
-      src: z.string(),
-      alt: z.string(),
-      credit: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().max(160),
+      publishedAt: z.date().optional(),
+      tags: z.array(z.string()).optional(),
+      cover: z.object({
+        src: image(),
+        alt: z.string(),
+        credit: z.string(),
+      }),
     }),
-  }),
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
