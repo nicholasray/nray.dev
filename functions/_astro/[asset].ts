959e9480344cf69ae6271d49e8236be5045f4d78
Nicholas Ray <nray@nray.dev>
2023-08-24 11:34:55 -0600

    Use test instead of match

--- functions/_astro/[asset].ts
+++ functions/_astro/[asset].ts
@@ -5,7 +5,7 @@ const OPTIMIZED_ASSET_DIR = `${ASSET_DIR}-transformed`;
 export const onRequest: PagesFunction = async ({ request, next, env }) => {
   const url = new URL(request.url);
 
-  if (!url.pathname.match(/\/_astro\/(.+)_(.+).(?:jpg|jpeg|png|webp|avif)/)) {
+  if (!/\/_astro\/(.+)_(.+).(?:jpg|jpeg|png|webp|avif)/.test(url.pathname)) {
     return next();
   }
 
