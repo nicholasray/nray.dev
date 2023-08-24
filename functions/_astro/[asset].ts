2b03db953489f9e74690479dd781402bff4f9079
Nicholas Ray <nray@nray.dev>
2023-08-24 01:04:41 -0600

    Add worker

---
+++ functions/_astro/[asset].ts
@@ -0,0 +1,39 @@
+import type { PagesFunction } from "@cloudflare/workers-types";
+const ASSET_DIR = "_astro";
+const OPTIMIZED_ASSET_DIR = `${ASSET_DIR}-transformed`;
+
+export const onRequest: PagesFunction = async ({ request, next, env }) => {
+  const url = new URL(request.url);
+
+  if (!url.pathname.match(/\/_astro\/(.+)_(.+).(?:jpg|jpeg|png|webp|avif)/)) {
+    return next();
+  }
+
+  const accept = request.headers.get("Accept");
+
+  // FROM: /_astro/avatar.19c5be45_1jNg6X.jpg
+  // TO:  /_astro-optimized/avatar.19c5be45_1jNg6X.avif
+  // TO:  /_astro-optimized/avatar.19c5be45_1jNg6X.webp
+  // TO:  /_astro-optimized/avatar.19c5be45_1jNg6X.jpg
+  function rewriteUrl(format: string) {
+    const file = request.url.split("/").pop()!;
+    const dotIdx = file.lastIndexOf(".");
+    const ext = file.substring(dotIdx);
+    const name = file.substring(0, dotIdx);
+    const newPath = `/${OPTIMIZED_ASSET_DIR}/${name}${`.${
+      format || ext.slice(1)
+    }`}`;
+
+    return env.ASSETS.fetch(new URL(newPath, request.url));
+  }
+
+  if (accept?.includes("image/avif")) {
+    return rewriteUrl("avif");
+  }
+
+  if (accept?.includes("image/webp")) {
+    return rewriteUrl("webp");
+  }
+
+  return rewriteUrl("");
+};
