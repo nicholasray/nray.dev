1902a2df5d1403678a589cb42496daa9a91cdab0
Nicholas Ray <nray@nray.dev>
2023-09-02 11:14:31 -0600

    Remove astro-icon and inline svgs instead

--- src/icons/heroicons.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { createIconPack } from "astro-icon/pack";
-
-export default createIconPack({ package: "heroicons", dir: "24/outline" });
