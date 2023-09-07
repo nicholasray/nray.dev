0931361d0261b5bb4d7abe57c23618d25ca39901
Nicholas Ray <nray@nray.dev>
2023-09-06 21:26:03 -0600

    Cleanup tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -32,19 +32,6 @@ module.exports = {
         performant: "opacity, transform",
       },
       screens: screens,
-      keyframes: {
-        "transform-zero": {
-          "100%": { transform: "translate(0)", opacity: 1 },
-        },
-        "vertical-bounce-inefficient": {
-          "0%, 100%": { top: 0 },
-          "50%": { top: "286px" },
-        },
-        "vertical-bounce-efficient": {
-          "0%, 100%": { transform: "translateY(0)" },
-          "50%": { transform: "translateY(286px)" },
-        },
-      },
     },
   },
   blocklist: ["container"],
