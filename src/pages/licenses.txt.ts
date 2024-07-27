aa2f723a6a645e28d0f3d1e6b5a82f74a23127ba
Nicholas Ray <nray@nray.dev>
2024-07-26 21:28:31 -0600

    Remove licenses

--- src/pages/licenses.txt.ts
+++
@@ -1,7 +0,0 @@
-import { getLicenseFileText } from "generate-license-file";
-
-export async function GET() {
-  const licenses: string = await getLicenseFileText("package.json");
-
-  return new Response(licenses);
-}
