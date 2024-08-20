8156852ad179b16d10438edfdd6e7f0c5d3a54cc
Nicholas Ray <nray@nray.dev>
2024-08-19 20:28:29 -0600

    Remove unused

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { createLoaders } from "./loaders/loaders";
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
-  context.locals.runtime.env.DB;
   context.locals.loaders = createLoaders(context.locals.runtime.env.DB);
 
   return next();
