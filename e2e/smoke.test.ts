3493820b0d0c783a06fef126982ac60cdff4ea8b
Nicholas Ray <nray@nray.dev>
2024-10-20 13:49:02 -0600

    Fix e2e tests

--- e2e/smoke.test.ts
+++ e2e/smoke.test.ts
@@ -2,6 +2,5 @@ import { expect, test } from "@playwright/test";
 
 test("App loads", async ({ page }) => {
   await page.goto("/");
-
-  expect(page).toHaveTitle("Nicholas Ray");
+  await expect(page).toHaveTitle("Nicholas Ray");
 });
