fc620da1431fca12700f472a3774b1805cdbf814
Nicholas Ray <nray@nray.dev>
2025-10-03 21:24:11 -0600

    Update dependency list

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,9 +2,15 @@ import { Kysely } from "kysely";
 import { LibsqlDialect } from "@libsql/kysely-libsql";
 import type { DB } from "./types";
 
+// console.log("******", process.env.DATABASE_URL);
+
+// setTimeout(() => {
+//   console.log(process.env.DATABASE_URL);
+// }, 5000);
+
 export const db = new Kysely<DB>({
   dialect: new LibsqlDialect({
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
     authToken: process.env.DATABASE_TOKEN,
   }),
 });
